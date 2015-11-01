7f776c8dd225c52da07711c4162bfae7b04964cf
Mike Klophaus <mmklophaus@gmail.com>
2015-10-31 17:40:33 -0700

    created user/sessions/welcome controllers along with views

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,31 @@
+class SessionsController < ApplicationController
+
+  def new
+
+  end
+
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_path, notice: 'Logged in to account!'
+    else
+      flash.now.alert = 'Invalid login credentials - try again!'
+
+      #starts over
+      render 'new'
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    ##goes to homepage
+    redirect_to root_path, notice: "Logged out!"
+  end
+
+
+
+
+
+end
+
