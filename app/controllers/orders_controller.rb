e90a4987013ad835ab41f4330268b9a499814440
Mike Klophaus <mmklophaus@gmail.com>
2015-11-07 18:53:19 -0800

    Styling changes

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,30 +1,27 @@
 class OrdersController < ApplicationController
 
   def index
-    @orders = current_user.
+    @orders = current_user.orders
   end
 
   def show
     #@orders = current_user.orders
-    @order = current_user.orders.find([params[:id]])
+    @order = cart
   end
 
   def new
    # @order = Order.find_by_id(params[:id])
     @order = Order.new
     @products = current_user.products
-
   end
 
   def create
     @order = current_user.orders.build order_params
-
     if @order.save
     #  redirect_to , notice: 'Product was successfully created.'
     else
       render :new
     end
-
   end
 
   def order_params
